54ed859deefbbf855ad4fdd11709200db345411b
Lentou <33175224+Lentou@users.noreply.github.com>
2021-04-03 10:46:22 +0800

    Clean Parts

--- src/Hytlenz/Main.php
+++ src/Hytlenz/Main.php
@@ -13,15 +13,16 @@ class Main extends PluginBase implements Listener{
   
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getLogger()->info("Information has been revealed");
+		$this->getLogger()->info("Information has been revealed - Lentou");
 		
 		@mkdir($this->getDataFolder());
-		$this->saveResource("config.yml");
-		$this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+		$this->saveDefaultConfig();
+		$this->getResource("config.yml");
+		$this->config = $this->getConfig()->getAll();
 	}
 	
 	public function onDisable(){
-		$this->getLogger()->info("Information has been leashed");
+		$this->getLogger()->info("Information has been vanished - Lentou");
 	}
 	
 	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
@@ -36,18 +37,18 @@ class Main extends PluginBase implements Listener{
 	public function infoForm($sender){
 		$form = new SimpleForm(function (Player $sender, $data) {
             		if (is_null($data)) return true;
-            		$buttons = array_keys($this->cfg->get("wiki"));
+            		$buttons = array_keys($this->config["wiki"]);
             		if (count($buttons) == $data) return;
             		$button = $buttons[$data];
 			$this->pageForm($sender, $button);
         	});
-        	$form->setTitle($this->cfg->getNested("wikipedia.title"));
-        	$form->setContent(implode("\n", str_replace("{player}", $sender->getName(), $this->cfg->getNested("wikipedia.content"))));
-        	foreach(array_keys($this->cfg->get("wiki")) as $wiki) {
+        	$form->setTitle($this->config["wikipedia"]["title"]);
+        	$form->setContent(implode("\n", str_replace("{player}", $sender->getName(), $this->config["wikipedia"]["content"])));
+        	foreach(array_keys($this->config["wiki"]) as $wiki) {
             		$form->addButton(
-				$this->cfg->getNested("wiki." . $wiki . ".button"),
-				$this->cfg->getNested("wiki." . $wiki . ".imgtype"), 
-				$this->cfg->getNested("wiki." . $wiki . ".imgpath")
+				$this->config["wiki"]["$wiki"]["button"][0],
+				$this->config["wiki"]["$wiki"]["button"][1],
+				$this->config["wiki"]["$wiki"]["button"][2]
 			);
         	}
         	$form->sendToPlayer($sender);
@@ -62,9 +63,9 @@ class Main extends PluginBase implements Listener{
 				break;
             		}
         	});
-        	$form->setTitle($this->cfg->getNested("wiki." . $button . ".title"));
-        	$form->setContent(implode("\n", $this->cfg->getNested("wiki." . $button . ".content")));
-        	$form->addButton($this->cfg->getNested("wikipedia.return"));
+        	$form->setTitle($this->config["wiki"]["$button"]["title"]);
+        	$form->setContent(implode("\n", $this->config["wiki"]["$button"]["content"]));
+        	$form->addButton($this->config["wikipedia"]["return"]);
         	$form->sendToPlayer($sender);
 	}
 	
